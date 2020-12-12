053edcc43dbaeb9e777ef504f6af1f9a0ecaa73d
aplchian <alex.boquis@gmail.com>
2020-12-12 16:21:18 -0500

    initial commit

---
+++ packages/lama-cms/schemas/transcriber.js
@@ -0,0 +1,12 @@
+export default {
+    name: 'transcriber',
+    title: 'Transcriber',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+    ]
+}
